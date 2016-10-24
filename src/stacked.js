1a44deb8011fee410472a1efa1611b480911f617
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2016-10-24 15:53:36 -0700

    Adds form for selecting date range

--- src/stacked.js
+++ src/stacked.js
@@ -47,6 +47,9 @@ if (access_token) {
             // print entire response for now
             console.log(data.response);
 
+            populateFormFields("from");
+            populateFormFields("to");
+
             loadCheckinTable(data.response);
 
         },
@@ -61,8 +64,62 @@ if (access_token) {
     $('#connection').html('<a href="' + auth_url + '" target="_new"><img src="connect4sq.png" alt="Connect to Foursquare"></a>');
 }
 
+// what to do on form update:
+$(function() {
+    var update = function() {
+        var updatedFormData = $('#dateForm').serializeArray()
+                                            .reduce(function(fullMap, miniMap) { 
+                                                        fullMap[miniMap.name] = miniMap.value;
+                                                        return fullMap;
+                                                    },
+                                                {});
+        console.log(updatedFormData);
+        var startDate = toEpoch(updatedFormData.fromYear, updatedFormData.fromMonth, 1);
+        console.log(startDate);
+        var endDate = toEpoch(updatedFormData.toYear, updatedFormData.toMonth, 1);
+        console.log(endDate);
+
+        if (endDate > startDate) {
+            console.log('success');
+        }
+        else {
+            console.log('From date of '+toDate(startDate)+' is AFTER end date of '+toDate(endDate));
+        }
+    };
+    update();
+    $('#dateForm').change(update);
+})
+
+function populateFormFields(prefix) {
+    dateRangeForm = d3.select('#'+prefix+'DateRangeForm');
+    d3.select('#'+prefix+'Date')
+        .append('h4')
+        .text(prefix);
+
+    fromDateFormMonth = d3.select('#'+prefix+'DateMonth')
+                        .append('select')
+                        .attr('class', 'form-control')
+                        .attr('name', prefix+'Month');;
+    for (var i = 0; i < 12; i++) {
+        fromDateFormMonth.append('option')
+                            .attr('value', i)
+                            .text(monthMap[i]);
+    }
+    fromDateFormYear = d3.select('#'+prefix+'DateYear')
+                            .append('select')
+                            .attr('class', 'form-control')
+                            .attr('name', prefix+'Year');
+    years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
+    for (var i = 0; i < years.length; i++) {
+        fromDateFormYear.append('option')
+                            .attr('value', years[i])
+                            .text(years[i]);
+    }
+}
+
 function loadCheckinTable(response) {
     var checkinItems = response.checkins.items;
+    var maxDisplayed = 10;
 
     checkinTable = d3.select('#checkin-table');
     header = checkinTable.append('thead');
@@ -74,7 +131,7 @@ function loadCheckinTable(response) {
        .text('Date');
 
     tableBody = checkinTable.append('tbody');
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < maxDisplayed; i++) {
         row = tableBody.append('tr');
         row.append('td')
            .text(checkinItems[i].venue.name)
@@ -82,7 +139,7 @@ function loadCheckinTable(response) {
         row.append('td')
            .text(monthMap[checkInDate.getMonth()] + ' ' + checkInDate.getFullYear());
     }
-    if (checkinItems.length > 20) {
+    if (checkinItems.length > maxDisplayed) {
         row = tableBody.append('tr');
         row.append('td')
            .text('...')
@@ -93,6 +150,11 @@ function loadCheckinTable(response) {
 
 function toDate(epochTime, correction=0) {
     return new Date((+epochTime + correction)*1000);
+}
+
+function toEpoch(year=2016, month=11, day=1) {
+    var dateObj = new Date(year,month,day);
+    return Math.floor(dateObj.getTime()/1000)
 }
 
 function parseStringFromURL(prefix='/') {
